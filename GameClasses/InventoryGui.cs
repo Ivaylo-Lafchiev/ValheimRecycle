38187ede3d9ae8a6a5ce85b8eb5c0fa7e4ec21f4
Ivaylo-Lafchiev <ivailolafchiev@yahoo.com>
2023-02-11 13:27:38 +0000

    Fix import

--- GameClasses/InventoryGui.cs
+++ GameClasses/InventoryGui.cs
@@ -2,7 +2,6 @@ using HarmonyLib;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static System.Xml.XmlWellFormedWriter.AttributeValueCache;
 
 namespace ValheimRecycle
 {
